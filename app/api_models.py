0f75611cdcbae41e5fbd602c9396174b16f6816e
YaliB <yali.babani@gmail.com>
2026-02-26 19:17:02 +0200

    explain from_attributes

--- app/api_models.py
+++ app/api_models.py
@@ -15,7 +15,7 @@ class UserResponse(UserBase):
     created_at: datetime
 
     class Config:
-        from_attributes = True
+        from_attributes = True # Allows Pydantic to maintain compatibility with ORM objects
 
 # --- Post Schemas ---
 class PostBase(BaseModel):
